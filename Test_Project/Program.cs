3daf68ca9292290fb43ec96871552c6c423747cd
Vencislav <vencislav67@hotmail.com>
2023-02-01 12:35:34 +0200

    Fibonacci

--- Test_Project/Program.cs
+++ Test_Project/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Hello, World!");
+int a = 1;
+int b = 1;
+int c = a + b;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+while (a+b < 1000)
+{
+    a = b;
+    b = c;
+    c = a + b;
+    Console.WriteLine(c);
+}
